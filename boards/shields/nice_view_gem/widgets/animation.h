2a6aeb44ea368a455f8e4bec352728333d085c4d
raeldev <raeldevgames@gmail.com>
2025-03-11 17:06:51 -0400

    Update animation.h

--- boards/shields/nice_view_gem/widgets/animation.h
+++ boards/shields/nice_view_gem/widgets/animation.h
@@ -4,4 +4,5 @@
 #include "util.h"
 #include "screen_peripheral.h"
 
-void draw_animation(lv_obj_t *canvas);
+void draw_left_animation(lv_obj_t *canvas);
+void draw_right_animation(lv_obj_t *canvas);
