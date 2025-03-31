1477f5c9d2f332e4e5e3811973db25b50fc128f5
raeldev <raeldevgames@gmail.com>
2025-03-31 18:19:28 -0400

    Adding profile/layer status to peripheral

--- boards/shields/nice_view_mix/widgets/screen_peripheral.h
+++ boards/shields/nice_view_mix/widgets/screen_peripheral.h
@@ -8,6 +8,8 @@ struct zmk_widget_screen {
     sys_snode_t node;
     lv_obj_t *obj;
     lv_color_t cbuf[BUFFER_SIZE * BUFFER_SIZE];
+    lv_color_t cbuf2[BUFFER_SIZE * BUFFER_SIZE];
+    lv_color_t cbuf3[BUFFER_SIZE * BUFFER_SIZE];
     struct status_state state;
     int64_t startup_time;
     int current_src_index;
